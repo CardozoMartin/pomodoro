9116dcb1f011716a645729b600ed09528d524968
Martin Cardozo <tiin.-@hotmail.es>
2024-02-08 00:15:53 -0300

    se crean los primeros estilos y variables de estados

--- App.js
+++ App.js
@@ -2,22 +2,24 @@ import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, Text, View, Platform, SafeAreaView } from "react-native";
 import Header from "./src/components/Header";
-
+const color = ["#F7DC6F","#A2D9CE","#D7BDE2"]
 export default function App() {
   const [isWorking, setIsWorking] = useState(false);
   const [time, setTime] = useState(25 * 60);
-  const [currtenTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+  const [currtenTime, setCurrenTime] = useState("POMO" | "SHORT" | "BREAK");
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, {backgroundColor:color[currtenTime]}]}>
       <View style={{ paddingTop: Platform.OS === "android" && 40 }}>
         <Text style={{ fontSize: 40 }}>Pomodoro</Text>
 
         <Header
           currtenTime={currtenTime}
-          setCurrentTime={setCurrentTime}
-          time={time}
+          setCurrenTime={setCurrenTime}
+          setTime={setTime}
         />
+        <Text style={{ fontSize: 40 }}>{time}</Text>
+
 
         <StatusBar style="auto" />
       </View>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const options = ["Pomodoro", "Short Break", "Long Break"];
-const Header = ({ time }) => {
+const Header = ({ setTime, currenTime, setCurrenTime }) => {
+  const handlePress = (index) => {
+    const newTime = index === 0 ? 25 : index === 1 ? 5 : 15;
+    setCurrenTime(index);
+    setTime(newTime * 60);
+  };
   return (
-    <View style={{flexDirection:"row"}}>
+    <View style={{ flexDirection: "row" }}>
       {options.map((item, index) => (
-        <TouchableOpacity key={index} style={style.itemStyle}>
+        <TouchableOpacity
+          key={index}
+          onPress={() => {
+            handlePress(index);
+          }}
+          style={[
+            style.itemStyle,
+            currenTime !== index && { borderColor: "black",color:"white" },
+          ]}
+        >
           <Text>{item}</Text>
         </TouchableOpacity>
       ))}
@@ -15,11 +29,15 @@ const Header = ({ time }) => {
 };
 
 const style = StyleSheet.create({
-    itemStyle:{
-        width:"33%",
-        borderWidth:3,
-        padding:5,
-    }
-})
+  itemStyle: {
+    width: "33%",
+    borderWidth: 3,
+    alignItems:"center",
+    padding: 5,
+    borderRadius:10,
+    borderBlockColor:"white",
+    marginVertical:20
+  },
+});
 
 export default Header;

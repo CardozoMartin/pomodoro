cc63f6435aa49cfedc753c183ed9f9d90580cc0b
Martin Cardozo <tiin.-@hotmail.es>
2024-02-08 15:22:32 -0300

    se termina proyecto de pomodoro en react native

--- App.js
+++ App.js
@@ -1,17 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { StyleSheet, Text, View, Platform, SafeAreaView } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Platform, SafeAreaView, TouchableOpacity } from "react-native";
 import Header from "./src/components/Header";
 import Timer from "./src/components/Timer";
-const color = ["#F7DC6F","#A2D9CE","#D7BDE2"]
+const color = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
 export default function App() {
   const [isWorking, setIsWorking] = useState(false);
   const [time, setTime] = useState(25 * 60);
   const [currtenTime, setCurrenTime] = useState("POMO" | "SHORT" | "BREAK");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(()=>{
+    let interval = null;
+
+    if(isActive){
+      interval = setInterval(()=>{
+        setTime(time-1)
+      },1000)
+    }else{
+      clearInterval(interval)
+    }
+
+    if(time === 0){
+      setIsActive(false);
+      setIsWorking((prev)=>!prev);
+      setTime(isWorking ? 300 : 1500)
+    }
+
+    return ()=> clearInterval(interval);
+  },[isActive,time])
+  const handleStarStop = ()=>{
+    setIsActive(!isActive)
+  }
 
   return (
-    <SafeAreaView style={[styles.container, {backgroundColor:color[currtenTime]}]}>
-      <View style={{flex:1,paddingHorizontal:15, paddingTop: Platform.OS === "android" && 40 }}>
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: color[currtenTime] }]}
+    >
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: 15,
+          paddingTop: Platform.OS === "android" && 40,
+        }}
+      >
         <Text style={{ fontSize: 40 }}>Pomodoro</Text>
 
         <Header
@@ -20,8 +52,9 @@ export default function App() {
           setTime={setTime}
         />
         <Timer time={time}></Timer>
-
-
+        <TouchableOpacity onPress={handleStarStop} style={styles.button}>
+          <Text  style={{color:"white", fontWeight:"bold"}}>{isActive ? "STOP" : "START"}</Text>
+        </TouchableOpacity>
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
@@ -33,4 +66,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
-}); 
+  button:{
+    alignItems:"center",
+    marginTop:15,
+    backgroundColor:"#333333",
+    padding:15,
+    borderRadius:15
+    
+  }
+});
